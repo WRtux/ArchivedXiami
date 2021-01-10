9f371a968b6ee9cd247601a19e2933a6ee3324fd
WRtux <wildernessrgr@aliyun.com>
2021-01-11 01:33:00 +0800

    Update genre capture script
    Fix some bugs and adopt some ES6 features.

--- source/genre.js
+++ source/genre.js
@@ -1,11 +1,11 @@
 //Ensure start from //emumo.xiami.com/genre
-var genreExp = new RegExp("(?<=genre/detail/gid/|genre/detail/gid/)[0-9]+");
-var artistExp = new RegExp("(?<=artist/)[0-9A-Za-z]+"), albumExp = new RegExp("(?<=album/)[0-9A-Za-z]+"), songExp = new RegExp("(?<=song/)[0-9A-Za-z]+");
+"use strict";
+
 (function () {
-	var cont = frame.contentDocument.getElementById("tree").getElementsByTagName("dl")[0];
-	var eles = cont.getElementsByTagName("dt");
-	for (var i = 0; i < eles.length; i++) {
-		var an = eles[i].getElementsByTagName("a")[0];
+	let cont = frame.contentDocument.getElementById("tree").getElementsByTagName("dl")[0];
+	let eles = cont.getElementsByTagName("dt");
+	for (let ele of eles) {
+		let an = ele.getElementsByTagName("a")[0];
 		queue.push({ type: "genre", url: an.href, callback: captureGenre, data: data });
 	}
 	frame.task = queue.shift();
@@ -13,50 +13,50 @@ var artistExp = new RegExp("(?<=artist/)[0-9A-Za-z]+"), albumExp = new RegExp("(
 })();
 
 function captureGenre(doc) {
-	var cont = doc.getElementById("texts");
-	var n = cont.getElementsByTagName("h3")[0].textContent.trim(),
+	let cont = doc.getElementById("texts");
+	let n = cont.getElementsByTagName("h3")[0].textContent.trim(),
 		txt = cont.getElementsByClassName("content")[0].textContent.trim();
-	var en = { type: this.type, id: location.href.match(genreExp)[0], name: n, description: txt };
-	en.songs = [];
+	let en = { type: this.type, id: this.url.match(genreExp)[0], name: n, description: txt };
+	en.songs = new Array();
 	cont = doc.getElementById("songs").getElementsByClassName("content")[0];
-	var eles = cont.getElementsByClassName("info");
-	for (var i = 0; i < eles.length; i++) {
-		var sng = eles[i].getElementsByTagName("a")[0], art = eles[i].getElementsByTagName("a")[1];
-		var sid = sng.href.match(songExp)[0], uid = art.href.match(artistExp)[0];
+	let eles = cont.getElementsByClassName("info");
+	for (let ele of eles) {
+		let sng = ele.getElementsByTagName("a")[0], art = ele.getElementsByTagName("a")[1];
+		let sid = sng.href.match(songExp)[0], uid = art && art.href.match(artistExp) && art.href.match(artistExp)[0];
 		en.songs.push({ id: sid, name: sng.textContent, artist: uid });
 		if (!pool.songs.has(sid))
 			pool.songs.set(sid, { id: sid, name: sng.textContent });
-		if (!pool.artists.has(uid))
+		if (uid && !pool.artists.has(uid))
 			pool.artists.set(uid, { id: uid, name: art.textContent });
 	}
-	en.albums = [];
+	en.albums = new Array();
 	cont = doc.getElementById("albums").getElementsByClassName("content")[0];
 	eles = cont.getElementsByClassName("info");
-	for (var i = 0; i < eles.length; i++) {
-		var alb = eles[i].getElementsByTagName("a")[0], art = eles[i].getElementsByTagName("a")[1];
-		var aid = alb.href.match(albumExp)[0], uid = art.href.match(artistExp)[0];
+	for (let ele of eles) {
+		let alb = ele.getElementsByTagName("a")[0], art = ele.getElementsByTagName("a")[1];
+		let aid = alb.href.match(albumExp)[0], uid = art && art.href.match(artistExp) && art.href.match(artistExp)[0];
 		en.albums.push({ id: aid, name: alb.textContent, artist: uid });
 		if (!pool.albums.has(aid))
 			pool.albums.set(aid, { id: aid, name: alb.textContent });
-		if (!pool.artists.has(uid))
+		if (uid && !pool.artists.has(uid))
 			pool.artists.set(uid, { id: uid, name: art.textContent });
 	}
-	en.artists = [];
+	en.artists = new Array();
 	cont = doc.getElementById("artists").getElementsByClassName("content")[0];
 	eles = cont.getElementsByClassName("info");
-	for (var i = 0; i < eles.length; i++) {
-		var art = eles[i].getElementsByTagName("a")[0];
-		var uid = art.href.match(artistExp)[0];
+	for (let ele of eles) {
+		let art = ele.getElementsByTagName("a")[0];
+		let uid = art.href.match(artistExp)[0];
 		en.artists.push({ id: uid, name: art.textContent });
 		if (!pool.artists.has(uid))
 			pool.artists.set(uid, { id: uid, name: art.textContent });
 	}
 	if (this.type == "genre") {
-		en.styles = [];
-		var cur = doc.getElementById("tree").getElementsByClassName("unfold current")[0],
+		en.styles = new Array();
+		let cur = doc.getElementById("tree").getElementsByClassName("unfold current")[0],
 			eles = cur.nextElementSibling.getElementsByTagName("li");
-		for (var i = eles.length - 1; i >= 0; i--) {
-			var an = eles[i].getElementsByTagName("a")[0];
+		for (let i = eles.length - 1; i >= 0; i--) {
+			let an = eles[i].getElementsByTagName("a")[0];
 			queue.unshift({ type: "style", url: an.href, callback: captureGenre, data: en.styles });
 		}
 	}
--- source/queue.js
+++ source/queue.js
@@ -1,3 +1,7 @@
+"use strict";
+var queue = [];
+var data = new Array();
+
 var frame = document.getElementById("interface"), download = document.getElementById("download");
 frame.onload = function () {
 	if (frame.task)
@@ -6,13 +10,10 @@ frame.onload = function () {
 		frame.task = queue.shift();
 		frame.src = frame.task.url;
 	} else if (data.length > 0) {
-		var blob = new Blob([JSON.stringify(data)], { type: "application/json" });
+		let blob = new Blob([JSON.stringify(data)], { type: "application/json" });
 		download.href = URL.createObjectURL(blob);
 		download.click();
-		data = [];
+		data = new Array();
 		console.log("Capture complete.");
 	}
 };
-
-var queue = [];
-var data = [];
