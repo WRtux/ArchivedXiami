8cca4141ed273238c1ed47d71028f7c55c67a0de
WRtux <wildernessrgr@aliyun.com>
2021-01-27 23:31:47 +0800

    Update pool script
    Fix a bug and add a function.

--- source/pool.js
+++ source/pool.js
@@ -5,31 +5,45 @@ for (let n in pool) {
 	pool[n].weigh = function (o) {
 		if (!o || (!o.sid && !o.id))
 			return;
-		let w = (this.get(o.sid || o.id) || { weight: 0 }).weight || 10;
-		if (!w || o.weight >= w) {
+		let w = (this.get(o.sid || o.id) || { weight: -1 }).weight || 11;
+		if (o.weight >= w) {
 			let en = new Object();
 			Object.assign(en, o);
 			this.set(o.sid || o.id, en);
+			return true;
 		}
+		return false;
 	};
 	pool[n].inflate = function (o) {
 		if (!o || (!o.sid && !o.id))
 			return;
-		let en = this.get(o.sid || o.id) || new Object();
-		delete en.referrer;
-		delete en.weight;
-		Object.assign(en, o);
-		this.set(o.sid || o.id, en);
+		let w = (this.get(o.sid || o.id) || { weight: -1 }).weight || 11;
+		if (!o.weight || o.weight >= w) {
+			let en = this.get(o.sid || o.id) || new Object();
+			delete en.referrer;
+			delete en.weight;
+			Object.assign(en, o);
+			this.set(o.sid || o.id, en);
+			return true;
+		}
+		return false;
 	};
 }
 
 function loadPool(dat) {
-	if (typeof dat == "string")
-		dat = JSON.parse(dat);
+	(typeof dat == "string") && (dat = JSON.parse(dat));
 	for (let n in dat) {
 		pool[n].clear();
 		for (let en of dat[n])
 			pool[n].set(en.sid || en.id, en);
+	}
+}
+
+function mergePool(dat) {
+	(typeof dat == "string") && (dat = JSON.parse(dat));
+	for (let n in dat) {
+		for (let en of dat[n])
+			pool[n].inflate(en);
 	}
 }
 
@@ -41,8 +55,4 @@ function savePool() {
 			dat[n].push(v);
 	}
 	downloadJSON(dat);
-}
-
-function inflatePool() {
-	//TODO
 }
