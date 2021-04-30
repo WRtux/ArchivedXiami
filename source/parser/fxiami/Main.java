0a4d3fd56e01b416c41d3c97544d4a700dc79831
WRtux <wildernessrgr@aliyun.com>
2021-04-30 17:11:39 +0800

    Add a parameter
    Add an action parameter.

--- source/parser/fxiami/Main.java
+++ source/parser/fxiami/Main.java
@@ -1,16 +1,40 @@
 package fxiami;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.List;
+
+import com.alibaba.fastjson.JSONArray;
+
+import fxiami.entry.Entry;
 
 public final class Main {
 	
 	public static void main(String[] args) throws IOException {
-		if (args.length != 2)
+		if (args.length != 3)
 			throw new IllegalArgumentException();
-//		File[] fs = new File(args[1]).listFiles();
-//		Exporter.exportJSONM(args[0], fs, new File(args[0] + ".jsonm"));
-		Parser.parseJSONM(args[0], new File(args[1]));
+		switch (args[0]) {
+		case "export":
+			File[] fs = new File(args[2]).listFiles();
+			Exporter.exportJSONM(args[1], fs, new File(args[1] + ".jsonm"));
+			break;
+		case "parse":
+			List<Entry> li = Parser.parseJSONM(args[1], new File(args[2]));
+			JSONArray arr = new JSONArray();
+			for (Entry en : li) {
+				arr.add(en.toJSON());
+			}
+			System.gc();
+			Writer wtr = new OutputStreamWriter(new FileOutputStream("parse.json"), "UTF-8");
+			wtr.write(arr.toJSONString());
+			wtr.close();
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 	
 	@Deprecated
